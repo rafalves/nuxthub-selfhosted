c38310949bd144b59e3e18dfd5c31e89504d5099
Rafael Alves <rafalves.costa@gmail.com>
2024-08-20 20:18:49 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col h-96 justify-center items-center">
+    <div class="text-4xl  font-semibold">Hi with <span class="underline text-red-600">TailwindCSS</span></div>
+
+    <pre class="mt-32"><code> {{ data || 'vazio' }}</code></pre>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+const data = ref('fazer')
+// const { data } = await useFetch('/api/teste')
+
+
+</script>
